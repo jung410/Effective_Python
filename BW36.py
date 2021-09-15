6ec563024c2911e3efede7cc17a44b96224915b0
JeongMinYoung <43362052+jung410@users.noreply.github.com>
2021-09-16 00:20:50 +0900

    BM38 commit

--- BW36.py
+++ BW36.py
@@ -2,6 +2,7 @@
 자식 프로세스를 관리하려면 subprocess를 사용하자.
 가장 간단한 방법은 내장 모듈 subprocess를 사용하는 것.
 '''
+import os
 import subprocess
 from time import time
 
@@ -27,3 +28,27 @@ for proc in procs:
     proc.communicate()
 end = time()
 print('Finished in %.3f seconds' % (end - start))
+
+
+def run_openssl(data):
+    env = os.environ.copy()
+    env['password'] = b'\xe24U\n\xd0Ql3S\x11'
+    proc = subprocess.Popen(
+        ['openssl', 'enc', '-des3', '-pass', 'env:password'],
+        env=env,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    proc.stdin.write(data)
+    proc.stdin.flush()
+    return proc
+
+procs = []
+for _ in range(3):
+    data = os.urandom(10)
+    proc = run_openssl(data)
+    procs.append(proc)
+
+for proc in procs:
+    out, err = proc.communicate()
+    print(out[-10:])
